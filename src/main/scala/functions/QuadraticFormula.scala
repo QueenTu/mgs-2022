99c2e240627b9c93892977b3499beab1c696dbbf
QueenTu <sydneyesmith06@gmail.com>
2022-06-08 13:14:08 +0000

    functional programming thingy

--- src/main/scala/functions/QuadraticFormula.scala
+++ src/main/scala/functions/QuadraticFormula.scala
@@ -1,44 +1,56 @@
 package functions
 
 import scala.math.sqrt
+// make sure you import things if you need to use a specific module because without this specific one 
+//-scala would not know what sqrt was, and its much easier to just import it rather then to make fuction.
 
+//when you click run it starts with main and goes back to the functions defined in object before it
 object QuadraticFormula {
   // TASK:
   //      Write a test to validate the following function.
 
   def quadraticFormula(a: Int, b: Int, c: Int): List[Double] = {
-    List(
-      (-b + sqrt(b * b - 4 * a * c)) / (2 * a),
-      (-b - sqrt(b * b - 4 * a * c)) / (2 * a)
-    ).distinct
-  }
- 
-  def main(argv: Array[String]): Unit = {
-    println("Hello this is main of QuadraticFormula")
-    println(quadraticFormula(1, 2, -3))
-    println(quadraticFormula(-1, 3, 7))
-  }
-  def discriminant(a: Int, b: Int, c: Int):Int = {
-    (b * b - (4 * a * c))
-   
-     val d = discriminant(a,b,c)
+    
+ // a double is with a decimal value and Int is just one    
+val d = discriminant(a,b,c)
 if ( d == 0)
   { 
-    println("one real solutions")
+   List(-b/(2*a))
   }
 else if ( d < 0) 
   {
-    println{"no real solutions"}
+    List()
   }
 
 else 
   {
-    println{"two real solutions"}
+    List(
+       -b + sqrt(d) / (2 * a),
+      -b - sqrt(d) / (2 * a)
+    ).sorted
+  // .sorted is to sort the list so that it is a;ways in ascending order regardless of negative or positive
+  // however when a is zero you get the value infinity, and if you try to solve for infinity you would always get thats not a number, how do you solve this?
   }
+
+        
+  }
+ 
+  
+  def discriminant(a: Int, b: Int, c: Int):Int = {
+    (b * b - (4 * a * c))
+   
   
   }
-}
 
+
+// array is an orderd list and storage and you can find an array by index, 
+// you cant do that with a list you would just go in order until you find what your looking for in that list
+ def main(argv: Array[String]): Unit = {
+    println("Hello this is main of QuadraticFormula")
+    println(quadraticFormula(0, 2, -3))
+    println(quadraticFormula(0, 3, 7))
+  }
+ 
   
   // TASK:
   //    Update the formula to compute the discriminant,
@@ -55,5 +67,5 @@ else
 
   
 
- 
+}
 
